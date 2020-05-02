9f4927d748587279f364a67e77a6737f8b0bb337
zuguorui <zu_guorui@126.com>
2020-05-02 18:52:40 +0800

    add sort

---
+++ sort/fast_sort.cpp
@@ -0,0 +1,11 @@
+#include "fast_sort.h"
+
+template<typename T>
+void fast_sort(T* src, int32_t len)
+{
+    if(len <= 1)
+    {
+        return;
+    }
+    
+}
---
+++ sort/fast_sort.h
@@ -0,0 +1,11 @@
+#ifndef _FAST_SORT_H_
+#define _FAST_SORT_H_
+
+
+#include<stdlib.h>
+#include<stdio.h>
+
+template<typename T>
+void fast_sort(T* src, int32_t len);
+
+#endif
